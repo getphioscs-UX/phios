08eb898939e01a8cdf9a1e0ad6030d38ca8c771c
getphioscs-UX <getphios.cs@gmail.com>
2026-09-25 13:53:12 +0800

    bazi: add R9 browser runtime entry

---
+++ assets/customer-ui/js/personal-products/bazi-r9-review-runtime.js
@@ -0,0 +1,3 @@
+import {renderVisualReportPages} from './visual-report-pages.js';
+import {fitPublicationForPrint,settlePublicationAssets} from './publication-report-pages.js';
+export {renderVisualReportPages,fitPublicationForPrint,settlePublicationAssets};
