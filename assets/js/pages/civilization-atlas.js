2525a747eb6aff08049daad94e972f312f9e7dc4
getphioscs-UX <getphios.cs@gmail.com>
2026-09-25 14:58:53 +0800

    Bind registry context into Atlas visual projection

--- assets/js/pages/civilization-atlas.js
+++ assets/js/pages/civilization-atlas.js
@@ -15,7 +15,7 @@ if(root){
     onLayerChange:activeLayer=>store.set(reconcileAtlasContextForLayer(activeLayer,store.get(),data),{source:'layer-nav'}),
     onStateChange:(patch,meta)=>store.set(patch,meta)
   });
-  renderAtlasStaticVisuals(root,{bindings:data.staticVisuals,state:store.get(),locale:getLocale()});};
+  renderAtlasStaticVisuals(root,{bindings:data.staticVisuals,state:store.get(),locale:getLocale(),data});};
   const unsubscribe=store.subscribe(render);
   const unbindLocale=onLocaleChange(()=>store.set({locale:getLocale()},{source:'locale'}));
   render();
